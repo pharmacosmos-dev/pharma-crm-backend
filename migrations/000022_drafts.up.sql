a688cb0a6e8f9c0f6a7f2a415de53dc9eaa8b24e
murtazokhongofurov <gofurovmurtazoxon@gmail.com>
2025-02-17 19:31:59 +0500

    fixed draft problem

--- migrations/000022_drafts.up.sql
+++ migrations/000022_drafts.up.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS "drafts" (
     "product_id" UUID REFERENCES products(id) ON DELETE CASCADE,
     "cash_box_id" UUID REFERENCES cash_boxes(id) ON DELETE CASCADE,
     "customer_id" UUID REFERENCES customers(id) ON DELETE CASCADE,
+    "store_id" UUID REFERENCES stores(id) ON DELETE CASCADE,
     "sale_id" UUID REFERENCES sales(id),
     "description" TEXT,
     "status" VARCHAR(50) DEFAULT 'pending',
