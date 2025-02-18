a1a971774d1f57cd9958f11f62149797222f4a12
murtazokhongofurov <gofurovmurtazoxon@gmail.com>
2025-02-18 09:48:51 +0500

    added text to check file extenssion

--- internal/controller/http/v1/upload.handler.go
+++ internal/controller/http/v1/upload.handler.go
@@ -65,7 +65,7 @@ func (h *UploadHandler) Upload(c *gin.Context) {
 	ext := filepath.Ext(file.File.Filename)
 	if ext != ".jpg" && ext != ".jpeg" && ext != ".png" {
 		h.log.Error("Invalid file type")
-		handleResponse(c, BadRequest, "Invalid file type")
+		handleResponse(c, BadRequest, "Invalid file type. Only .jpg, .jpeg, and .png files are allowed.")
 		return
 	}
 
