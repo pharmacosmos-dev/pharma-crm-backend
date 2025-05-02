96a06ec472c4b865d472f9c05a92328e805648d0
murtazokhongofurov <gofurovmurtazoxon@gmail.com>
2025-05-03 00:38:46 +0500

    fix: fixed quantity with unit_quantity if import details has fractional numbers

---
+++ migrations/000089_trigger_products_name_tsvector.down.sql
@@ -0,0 +1,2 @@
+DROP FUNCTION IF EXISTS update_name_tsvector();
+DROP TRIGGER IF EXISTS trg_update_name_tsvector ON products;
--- migrations/000089_trigger_products_name_tsvector.up.sql
+++ migrations/000089_trigger_products_name_tsvector.up.sql
@@ -1,13 +1,11 @@
-DROP FUNCTION IF EXISTS update_name_tsvector();
-CREATE FUNCTION update_name_tsvector() RETURNS trigger AS $$
+CREATE FUNCTION IF NOT EXISTS update_name_tsvector() RETURNS trigger AS $$
 BEGIN
   NEW.name_tsvector := to_tsvector('simple', NEW.name);
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trg_update_name_tsvector ON products;
-CREATE TRIGGER trg_update_name_tsvector
+CREATE TRIGGER IF NOT EXISTS trg_update_name_tsvector
 BEFORE INSERT OR UPDATE ON products
 FOR EACH ROW
 EXECUTE FUNCTION update_name_tsvector();
