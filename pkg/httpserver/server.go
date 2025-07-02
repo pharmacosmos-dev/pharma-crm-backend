9ce2d2a5b72de383723c8ab09a4de20dad25e69a
murtazokhongofurov <gofurovmurtazoxon@gmail.com>
2025-07-02 10:38:32 +0500

    feat: changed defaultWriteTimeout from 60 second to 120 second

--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -9,7 +9,7 @@ import (
 
 const (
 	_defaultReadTimeout     = 5 * time.Second
-	_defaultWriteTimeout    = 60 * time.Second
+	_defaultWriteTimeout    = 120 * time.Second
 	_defaultAddr            = ":80"
 	_defaultShutdownTimeout = 3 * time.Second
 )
