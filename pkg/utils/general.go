9fc2c8e196ad14718fc9f3fae16e7d6e53c1cbed
Jons <ozodjon160605@gmail.com>
2025-07-03 16:19:56 +0500

    feat: fixed dashboard

--- pkg/utils/general.go
+++ pkg/utils/general.go
@@ -142,12 +142,10 @@ func BeforeDatesTime(startDate, endDate time.Time) (time.Time, time.Time) {
 	diff := endDate.Sub(startDate)
 	if diff == 0 {
 		diff = 24 * time.Hour // kamida 1 kun
-	} else {
-		diff += 24 * time.Hour // yakuniy kunga qo‘shib hisoblaymiz
 	}
-
+	fmt.Println("diff", diff)
 	beforeStart := startDate.Add(-diff)
-	beforeEnd := startDate.Add(-time.Hour * 24)
+	beforeEnd := endDate.Add(-diff)
 
 	return beforeStart, beforeEnd
 }
