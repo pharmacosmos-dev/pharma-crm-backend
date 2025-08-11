efb0f4781d2398d9d2aaae81699f8e23fecb55d0
murtazokhongofurov <gofurovmurtazoxon@gmail.com>
2025-08-11 12:05:33 +0500

    feat: Changed run migration script

--- scripts/run_migration.sh
+++ scripts/run_migration.sh
@@ -14,6 +14,11 @@ fi
 # Construct full DB URL
 DB_URL="postgres://${PG_USER}:${PG_PASS}@${PG_HOST}:${PG_PORT}/${PG_DB}?sslmode=disable"
 
+# Optional: Reset dirty flag and force to latest migration version
+LATEST_VERSION=$(ls /app/migrations | grep -E '^[0-9]+' | sort -n | tail -1 | cut -d'_' -f1)
+echo "Forcing migration version to $LATEST_VERSION"
+migrate -path /app/migrations -database "$DB_URL" force "$LATEST_VERSION"
+
 # Run migrations
 migrate -path /app/migrations -database "$DB_URL" up
 
