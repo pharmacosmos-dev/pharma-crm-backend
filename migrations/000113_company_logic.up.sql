ba77f317533e401aca6349dc365b57cce0c560b0
Jons <ozodjon160605@gmail.com>
2025-09-04 23:08:09 +0500

    feat: little change in migration

--- migrations/000113_company_logic.up.sql
+++ migrations/000113_company_logic.up.sql
@@ -7,5 +7,9 @@ ALTER TABLE IF EXISTS store_products
 -- Add company_id to employees
 ALTER TABLE IF EXISTS employees
     ADD COLUMN IF NOT EXISTS company_id uuid REFERENCES companies ON DELETE CASCADE;
+-- Add company_id to excluded_products
 ALTER TABLE IF EXISTS excluded_products
+    ADD COLUMN IF NOT EXISTS company_id uuid REFERENCES companies ON DELETE CASCADE;
+-- Add company_id to product_bonuses
+ALTER TABLE IF EXISTS product_bonuses
     ADD COLUMN IF NOT EXISTS company_id uuid REFERENCES companies ON DELETE CASCADE;
