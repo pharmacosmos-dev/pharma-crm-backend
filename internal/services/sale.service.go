2061fbcd572d0239f800491653b77ce9ddc0ae61
mrgofurov <gofurovmurtazoxon@gmail.com>
2026-03-31 15:30:36 +0500

    feat: add PackageName to BarcodeResponse and implement fallback lookup by ProductId in sale service

--- internal/services/sale.service.go
+++ internal/services/sale.service.go
@@ -2381,7 +2381,7 @@ func (s *Services) GetDatasByMarkings(ctx context.Context, tx *gorm.DB, markings
 
 					var br domain.BarcodeResponse
 					err = tx.Table("product_barcodes pb").
-						Select("pb.id, pb.barcode, pb.mxik, pb.unit_code").
+						Select("pb.id, pb.barcode, pb.mxik, pb.unit_code, pb.unit_label AS package_name").
 						Where("pb.barcode = ? AND pb.status = 'completed' AND pb.mxik is not null AND pb.unit_code is not null ", barcode).
 						Order("pb.created_at desc").
 						Limit(1).
@@ -2389,25 +2389,29 @@ func (s *Services) GetDatasByMarkings(ctx context.Context, tx *gorm.DB, markings
 					if err != nil {
 						return nil, err
 					}
+
+					if br.Id == "" {
+						err = tx.Table("product_barcodes pb").
+							Select("pb.id, pb.barcode, pb.mxik, pb.unit_code, pb.unit_label AS package_name").
+							Where("pb.product_id = ? AND pb.status = 'completed' AND pb.mxik is not null AND pb.unit_code is not null ", cartItem.ProductId).
+							Order("pb.created_at desc").
+							Limit(1).
+							Scan(&br).Error
+						if err != nil {
+							return nil, err
+						}
+					}
+
 					br.CartItemId = m.Id
 					items = append(items, br)
 				}
 			}
 		} else {
-			// 3. Agar MarkingList bo‘lmasa → store_product_id orqali barcode topamiz
-			var productID string
-			err = tx.Table("store_products").
-				Select("product_id").
-				Where("id = ?", cartItem.StoreProductId).
-				Scan(&productID).Error
-			if err != nil {
-				return nil, err
-			}
 
 			var br domain.BarcodeResponse
 			err = tx.Table("product_barcodes pb").
-				Select("pb.id, pb.barcode, pb.mxik, pb.unit_code").
-				Where("pb.product_id = ? AND pb.status = 'completed' AND pb.mxik is not null AND pb.unit_code is not null ", productID).
+				Select("pb.id, pb.barcode, pb.mxik, pb.unit_code, pb.unit_label AS package_name").
+				Where("pb.product_id = ? AND pb.status = 'completed' AND pb.mxik is not null AND pb.unit_code is not null ", cartItem.ProductId).
 				Order("pb.created_at desc").
 				Limit(1).
 				Scan(&br).Error
