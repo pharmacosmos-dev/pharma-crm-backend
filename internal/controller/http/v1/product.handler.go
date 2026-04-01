57beaa158bc0401f1b0d3e3ec92b45fada6420ac
mrgofurov <gofurovmurtazoxon@gmail.com>
2026-04-01 12:59:33 +0500

    refactor: fix product barcode route indentation, simplify authorization logic, and update product retrieval service call

--- internal/controller/http/v1/product.handler.go
+++ internal/controller/http/v1/product.handler.go
@@ -84,10 +84,10 @@ func (h *ProductHandler) ProductRoutes(r *gin.RouterGroup) {
 		product.GET("/movement-units-export", h.ExportMovementUnits)
 		product.PUT("/update-ostatok/:store_product_id", h.UpdateOstatok)
 		product.POST("/barcode/upsert", h.CreateOrUpdateBarcodes)
-		product.GET("/:id/barcodes",    h.GetProductBarcodes)
-    	product.PUT("/:id/barcodes",    h.UpdateProductBarcode)
-		product.POST("/:id/barcodes",    h.CreateProductBarcode)
-    	product.DELETE("/:id/barcodes", h.DeleteProductBarcode)
+		product.GET("/:id/barcodes", h.GetProductBarcodes)
+		product.PUT("/:id/barcodes", h.UpdateProductBarcode)
+		product.POST("/:id/barcodes", h.CreateProductBarcode)
+		product.DELETE("/:id/barcodes", h.DeleteProductBarcode)
 	}
 }
 
@@ -194,39 +194,39 @@ func (h *ProductHandler) List(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), constants.DefaultContextTimeout)
 	defer cancel()
 
-	switch {
-		case user.Role == constants.RoleFranchise:
-			// franchise -> barcha store’lari
-			storeIds, err := h.service.GetStoreIdsByCompanyId(ctx, user.CompanyId)
-			if err != nil {
-				handleServiceResponse(c, nil, err)
-				return
-			}
-			params.StoreIds = storeIds
-			params.CompanyId = user.CompanyId
+	// switch {
+	// 	case user.Role == constants.RoleFranchise:
+	// 		// franchise -> barcha store’lari
+	// 		storeIds, err := h.service.GetStoreIdsByCompanyId(ctx, user.CompanyId)
+	// 		if err != nil {
+	// 			handleServiceResponse(c, nil, err)
+	// 			return
+	// 		}
+	// 		params.StoreIds = storeIds
+	// 		params.CompanyId = user.CompanyId
 
-		case !helper.IsAdmin(user):
-			// oddiy employee
-			if user.StoreId != "" {
-				params.StoreId = user.StoreId
-			}
-			params.CompanyId = user.CompanyId
+	// 	case !helper.IsAdmin(user):
+	// 		// oddiy employee
+	// 		if user.StoreId != "" {
+	// 			params.StoreId = user.StoreId
+	// 		}
+	// 		params.CompanyId = user.CompanyId
 
-		// admin -> filter yo‘q (hammasi)
-	}
-
-	// if !helper.IsAdmin(user) {
-	// 	if user.StoreId != "" {
-	// 		params.StoreId = user.StoreId
-	// 	}
-	// 	params.CompanyId = user.CompanyId
+	// 	// admin -> filter yo‘q (hammasi)
 	// }
+
+	if !helper.IsAdmin(user) {
+		if user.StoreId != "" {
+			params.StoreId = user.StoreId
+		}
+		params.CompanyId = user.CompanyId
+	}
 
 	// Pagination parameters
 	params.Limit, params.Offset = defaultLimitOffset(params.Limit, params.Offset)
 
 	// get products list
-	products, totalCount, err := h.service.GetProductsV2(ctx, &params)
+	products, totalCount, err := h.service.GetProducts(ctx, &params)
 	if err != nil {
 		handleServiceResponse(c, InternalError, err)
 		return
@@ -2976,7 +2976,6 @@ func (h *ProductHandler) getProductLock(productId string) *sync.Mutex {
 	return lock.(*sync.Mutex)
 }
 
-
 // CreateOrUpdateBarcodes godoc
 // @Summary Upsert product barcodes and material codes
 // @Description Save product barcode and material code data from product barcodes
@@ -3011,7 +3010,6 @@ func (h *ProductHandler) CreateOrUpdateBarcodes(c *gin.Context) {
 
 	handleResponse(c, OK, "SUCCESS")
 }
-
 
 // GetProductBarcodes godoc
 // @Summary      Get product barcodes
@@ -3101,8 +3099,6 @@ func (h *ProductHandler) CreateProductBarcode(c *gin.Context) {
 
 	handleResponse(c, OK, "CREATED")
 }
-
-
 
 // UpdateProductBarcodes godoc
 // @Summary      Update product barcode
