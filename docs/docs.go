c10d36e46bcc58ef0883c754d02159be72b86413
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2026-05-04 20:00:58 +0500

    feat: swagger

--- docs/docs.go
+++ docs/docs.go
@@ -32710,7 +32710,6 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-
     Version:          "1.0",
 	Host:             "",
 	BasePath:         "/v1",
