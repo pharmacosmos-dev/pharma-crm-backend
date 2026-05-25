c7647ee1bb6c47e40f2b80affb2fb42c13259de3
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2026-05-25 16:16:25 +0500

    feat: products history bug fixed and add metadata

--- domain/import.model.go
+++ domain/import.model.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/pharma-crm-backend/pkg/utils"
@@ -212,9 +213,10 @@ type ImportProductData struct {
 	UnitPerPack int             `gorm:"unit_per_pack" json:"-"`
 	Quantity    float64         `gorm:"quantity" json:"quantity"`
 	Sum         float64         `gorm:"sum" json:"sum"`
-	Name        string     `gorm:"name" json:"name"`
-	Status      string     `gorm:"status" json:"status"`
-	StoreName   string     `gorm:"store_name" json:"store_name"`
+	Name        string          `gorm:"name" json:"name"`
+	Status      string          `gorm:"status" json:"status"`
+	Metadata    json.RawMessage `gorm:"metadata" json:"metadata"`
+	StoreName   string          `gorm:"store_name" json:"store_name"`
 	CreatedAt   *time.Time      `gorm:"created_at" json:"created_at"`
 	TotalCount  int64           `gorm:"total_count" json:"-"`
 }
