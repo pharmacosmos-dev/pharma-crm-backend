c96e2e3d4e545824a726825b03824eb5e2f0cf28
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2026-06-25 11:44:03 +0500

    fix: update transfer query conditions to include 'failed_sent_to_1c' status

--- internal/services/product.service.go
+++ internal/services/product.service.go
@@ -1980,7 +1980,7 @@ vozvrat_data AS (
         JOIN transfers tr ON td.transfer_id = tr.id
         JOIN var_data vd ON td.product_id = vd.product_id
         JOIN stores s ON s.id = tr.from_store_id
-    WHERE (tr.status = 'completed' OR tr.status = 'sent-to-1c') AND tr.entry_type = 2
+    WHERE (tr.status = 'completed' OR tr.status = 'sent-to-1c' OR tr.status = 'failed_sent_to_1c') AND tr.entry_type = 2
         %s
     GROUP BY vd.unit_per_pack
 ),
@@ -1995,7 +1995,7 @@ transfer_data AS (
         JOIN var_data vd ON td.product_id = vd.product_id
         JOIN stores fs ON fs.id = tr.from_store_id
         JOIN stores ts ON ts.id = tr.to_store_id
-    WHERE (tr.status = 'completed' OR tr.status = 'sent-to-1c') AND tr.entry_type = 1
+    WHERE (tr.status = 'completed' OR tr.status = 'sent-to-1c' OR tr.status = 'failed_sent_to_1c') AND tr.entry_type = 1
         %s
     GROUP BY vd.unit_per_pack
 ),
