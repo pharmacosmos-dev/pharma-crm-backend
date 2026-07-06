6321d50ce4701849bc5c8ff2ac0c13780d34d8fd
Lazizbek <102922781+MLA2317@users.noreply.github.com>
2026-07-06 15:54:57 +0500

    feat: add LoyaltyCardAmount to Excel report and adjust column positions

--- internal/controller/http/v1/report.handler.go
+++ internal/controller/http/v1/report.handler.go
@@ -585,7 +585,7 @@ func (h *ReportHandler) GetProductsReportExcel(c *gin.Context) {
 	f.SetSheetName("Sheet1", sheetName)
 
 	// Headerlar
-	headers := []string{"ID", "Филиал", "Наименование", "Производитель", "Серия", "Срок Годности", "Кол-во", "Цена прихода", "Цена продажная", "Сумма прихода", "Сумма продажная", "Сумма наценки", "Сумма НДС", "Общее Сумма скидка", "Дата продажи", "Время продажи", "Пользователь", "ID ЧЕКА", "МК кол-во"}
+	headers := []string{"ID", "Филиал", "Наименование", "Производитель", "Серия", "Срок Годности", "Кол-во", "Цена прихода", "Цена продажная", "Сумма прихода", "Сумма продажная", "Сумма наценки", "Сумма НДС", "Общее Сумма скидка", "Накопительная", "Дата продажи", "Время продажи", "Пользователь", "ID ЧЕКА", "МК кол-во"}
 
 	err = setExcelHeaders(f, sheetName, headers)
 	if err != nil {
@@ -615,11 +615,12 @@ func (h *ReportHandler) GetProductsReportExcel(c *gin.Context) {
 		f.SetCellValue(sheetName, "L"+row, value.MarkupSum)
 		f.SetCellValue(sheetName, "M"+row, value.VatSum)
 		f.SetCellValue(sheetName, "N"+row, value.TotalDiscount)
-		f.SetCellValue(sheetName, "O"+row, value.CompletedAt.Add(time.Hour*5).Format(time.DateOnly))
-		f.SetCellValue(sheetName, "P"+row, value.CompletedAt.Add(time.Hour*5).Format(time.TimeOnly))
-		f.SetCellValue(sheetName, "Q"+row, value.FullName)
-		f.SetCellValue(sheetName, "R"+row, helper.SaleTypeToRussian(value.SaleType, value.SaleNumber))
-		f.SetCellValue(sheetName, "S"+row, value.MarkingCount)
+		f.SetCellValue(sheetName, "O"+row, value.LoyaltyCardAmount)
+		f.SetCellValue(sheetName, "P"+row, value.CompletedAt.Add(time.Hour*5).Format(time.DateOnly))
+		f.SetCellValue(sheetName, "Q"+row, value.CompletedAt.Add(time.Hour*5).Format(time.TimeOnly))
+		f.SetCellValue(sheetName, "R"+row, value.FullName)
+		f.SetCellValue(sheetName, "S"+row, helper.SaleTypeToRussian(value.SaleType, value.SaleNumber))
+		f.SetCellValue(sheetName, "T"+row, value.MarkingCount)
 	}
 
 	saveExcelToUploads(c, f, *h.log, "Sale_details")
